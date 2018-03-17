f91889b91e90f8a4f0c39f29c90c240aefd07a6d
KrzysztofJopek <krzysztofjopek99@gmail.com>
2018-03-17 19:11:05 +0100

    remove test func, add example

--- main.c
+++ main.c
@@ -3,7 +3,17 @@
 
 int main(int argc, char* argv[])
 {
+	int* p1 = (int*)halloc(100*sizeof(int));
 
-	test();
+	for(int i=1, *p=p1; i<=100; i++)
+		*p++ = i;
+
+	for(int i=1, *p=p1; i<=100; i++){
+		printf("%d ", *p++);
+		if(!(i%10))
+			printf("\n");
+	}
+
+	hfree(p1);
 	return 0;
 }
